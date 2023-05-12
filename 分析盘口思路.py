38c750ae8e24672684487a2491f71132cff64618
陈昭良 <295183917@qq.com>
2023-05-12 09:26:59 +0800

    更新结果分析

--- 分析盘口思路.py
+++ 分析盘口思路.py
@@ -849,9 +849,9 @@ def analyse_match():
         match_time = time.strptime(match_time, "%Y-%m-%d %H:%M")
         current_time = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
         time_diff = time.mktime(match_time) - time.mktime(time.strptime(current_time, "%Y-%m-%d %H:%M"))
-        if time_diff < 0:
+        if time_diff < -60 * 60:
             continue
-        if time_diff > 3600 * 5 and is_start == "未":
+        if time_diff > 3600 * 2 and is_start == "未":
             break
         is_friend = tr.xpath("./td[2]/a/text()")
         if len(is_friend) <= 0:
