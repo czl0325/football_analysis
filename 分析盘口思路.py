8a02083769005118027af69058e6157cc35659cd
czl <295183917@qq.com>
2023-06-06 18:43:53 +0800

    更新结果分析

--- 分析盘口思路.py
+++ 分析盘口思路.py
@@ -76,7 +76,7 @@ asia_map = {
 }
 match_map = {
     # "group1": [],
-    "group2": ["英超", "英甲", "西甲", "意甲", "法甲", "德甲", "德丙联", "葡超", "荷甲", "英冠", "英足总杯", "欧冠", "欧罗巴", "德国杯", "意超杯", "意杯", "意乙", "意丙1A", "意丙1B", "意丙1C", "意青联", "法国杯", "西班牙杯", "非洲杯", "欧洲杯", "解放者杯", "西乙", "西丙1", "西丙2", "西丙3", "西丙4", "西协甲", "法乙", "德乙", "荷乙", "比杯", "瑞典超", "瑞典甲", "丹超", "瑞士超", "克罗甲", "塞甲联", "波甲", "波乙", "苏冠", "苏甲", "土甲", "苏超", "瑞士甲", "丹甲", "罗甲", "英乙", "比乙", "保超", "挪超", "挪甲", "德东北", "爱超", "爱甲", "黑山甲", "阿巴超", "埃及甲", "摩洛超", "阿尔及甲", "捷甲", "捷克乙", "黑山甲", "阿甲", "巴甲", "巴圣锦", "巴乙", "阿乙", "乌拉超", "智利甲", "厄瓜多尔甲", "秘鲁甲", "巴拉圭联", "墨西联", "墨西乙", "哥斯甲", "哥甲", "美职联", "澳超", "日职", "韩足杯", "日职乙", "日联杯", "K1联赛", "K2联赛", "印度超", "印度甲", "印尼超", "澳南超", "澳维超", "越南联", "伊朗超", "伊朗甲", "阿联超", "马来超", "泰超", "巴西杯", "欧会杯", "乌兹超", "冰岛联", "沙特联", "波黑超", "澳昆超", "中超", "中甲", "中协杯", "智甲", "智乙", "斯洛文甲", "南非超", "冰岛超", "卡塔联", "亚冠杯", "希腊超A", "厄甲", "奥甲", "奥乙", "巴拉联", "芬超", "南俱杯", "突尼斯甲", "挪威杯"],
+    "group2": ["英超", "英甲", "西甲", "意甲", "法甲", "德甲", "德丙联", "葡超", "荷甲", "英冠", "英足总杯", "欧冠", "欧罗巴", "德国杯", "意超杯", "意杯", "意乙", "意丙1A", "意丙1B", "意丙1C", "意青联", "法国杯", "西班牙杯", "非洲杯", "欧洲杯", "解放者杯", "西乙", "西丙1", "西丙2", "西丙3", "西丙4", "西协甲", "法乙", "德乙", "荷乙", "比杯", "瑞典超", "瑞典甲", "丹超", "瑞士超", "克罗甲", "塞甲联", "波甲", "波乙", "苏冠", "苏甲", "土甲", "苏超", "瑞士甲", "丹甲", "罗甲", "英乙", "比乙", "保超", "挪超", "挪甲", "德东北", "爱超", "爱甲", "黑山甲", "阿巴超", "埃及甲", "摩洛超", "阿尔及甲", "捷甲", "捷克乙", "黑山甲", "阿甲", "巴甲", "巴圣锦", "巴乙", "阿乙", "乌拉超", "智利甲", "厄瓜多尔甲", "秘鲁甲", "巴拉圭联", "墨西联", "墨西乙", "哥斯甲", "哥甲", "美职联", "澳超", "日职", "韩足杯", "日职乙", "日联杯", "K1联赛", "K2联赛", "印度超", "印度甲", "印尼超", "澳南超", "澳维超", "越南联", "伊朗超", "伊朗甲", "阿联超", "马来超", "泰超", "巴西杯", "欧会杯", "乌兹超", "冰岛联", "沙特联", "波黑超", "澳昆超", "中超", "中甲", "中协杯", "智甲", "智乙", "斯洛文甲", "南非超", "冰岛超", "卡塔联", "亚冠杯", "希腊超A", "厄甲", "奥甲", "奥乙", "巴拉联", "芬超", "南俱杯", "突尼斯甲", "挪威杯", "俄超", "葡杯"],
     "group_inaccuracy": ["乌超", "葡甲", "比甲", "土超", "法丙"]
 }
 error_odds = Decimal('0.02')
@@ -438,12 +438,10 @@ def parse_asia(match, url):
                                     result_dic["输"] += 1
                                 else:
                                     result_dic["走"] += 1
-                        print(
-                            f"\033[1;30;45m该盘口下主队赢盘概率为{round(result_dic['赢'] / (result_dic['赢'] + result_dic['输']) * 100, 2)}%，{result_dic}\033[0m")
+                        print(f"\033[1;30;45m该盘口下主队赢盘概率为{round(result_dic['赢'] / (result_dic['赢'] + result_dic['输']) * 100, 2)}%，{result_dic}\033[0m")
             if match["home_team_rank"] >= match["visit_team_rank"] + 10 and match["visit_score"] >= match["home_score"] + 15:
                 if match["instant_pan_most"] <= 0:
-                    print(
-                        f"\033[1;30;45m警惕：主队排名{match['home_team_rank']}，客队排名{match['visit_team_rank']}，两者分差{match['visit_score'] - match['home_score']}，让球{match['instant_pan_most']}，客队盘口非常便宜！\033[0m")
+                    print(f"\033[1;30;45m警惕：主队排名{match['home_team_rank']}，客队排名{match['visit_team_rank']}，两者分差{match['visit_score'] - match['home_score']}，让球{match['instant_pan_most']}，客队盘口非常便宜！\033[0m")
                     result_dic = {
                         "赢": 0,
                         "输": 0,
@@ -676,6 +674,8 @@ def parse_asia(match, url):
             result_str += "输盘概率最大,为{:.2f}%".format(
                 all_lose_count / (all_win_count + all_lose_count + all_run_count) * 100)
             print(f"\033[1;34m{result_str}\033[0m")
+        else:
+            print(result_str)
     if league_win_count + league_run_count + league_lose_count > 0:
         result_str = f"亚盘本联赛盘口:赢={league_win_count},输={league_lose_count},走={league_run_count},"
         if league_win_count > league_lose_count and league_win_count > league_run_count:
@@ -690,6 +690,8 @@ def parse_asia(match, url):
             result_str += "输盘概率最大,为{:.2f}%".format(
                 league_lose_count / (league_win_count + league_lose_count + league_run_count) * 100)
             print(f"\033[1;34m{result_str}\033[0m")
+        else:
+            print(result_str)
     if len(score_map):
         new_score_map = {}
         for key in score_map:
@@ -973,7 +975,7 @@ def analyse_match():
         time_diff = time.mktime(match_time) - time.mktime(time.strptime(current_time, "%Y-%m-%d %H:%M"))
         if time_diff < -3600 * 1:
             continue
-        if time_diff > 3600 * 1 and is_start == "未":
+        if time_diff > 3600 * 5 and is_start == "未":
             break
         is_friend = tr.xpath("./td[2]/a/text()")
         if len(is_friend) <= 0:
@@ -999,7 +1001,7 @@ def analyse_match():
             print(f"\033[1;32m{match_item['match_group']}, 主队:{match_item['home_team']}, 客队:{match_item['visit_team']}, 比赛时间:{match_item['match_time']}。已结束比分: {match_item['field_score']}\033[0m")
         else:
             print(f"\033[1;31m{match_item['match_group']}, 主队:{match_item['home_team']}, 客队:{match_item['visit_team']}, 比赛时间:{match_item['match_time']}。\033[0m")
-        if match_item["match_category"] in match_map["group_inaccuracy"]:
+        if "match_category" in match_item and match_item["match_category"] in match_map["group_inaccuracy"]:
             print("不准确的联赛，暂不预测")
             continue
         # match_item = parse_europe(match_item, detail_url.replace("shuju", "ouzhi"))
@@ -1026,7 +1028,7 @@ def analyse_detail(detail_url):
 
 if __name__ == '__main__':
     analyse_match()
-    # analyse_detail("https://odds.500.com/fenxi/shuju-1084103.shtml")
+    # analyse_detail("https://odds.500.com/fenxi/shuju-1091156.shtml")
 
 # 热那亚 https://odds.500.com/fenxi/shuju-1055325.shtml
 # 墨尔本骑士 https://odds.500.com/fenxi/shuju-1075552.shtml
